6731a81d45aef2a5c7202a3b645158621cf6e1db
Batinh <lebatinh1505@gmail.com>
2024-01-10 21:07:34 +0700

    first commit

--- src/components/product_detail/Product_detail.js
+++ src/components/product_detail/Product_detail.js
@@ -1,5 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CardTitle,
+  Col,
+} from "reactstrap";
 
-export default function Product_detail() {
-    return <div>Product_detail</div>;
+export default function Product_detail(props) {
+  const { product } = props;
+  return (
+      <Col sm={6} md={4} lg={3}>
+        <div className="product_detail">
+          <Link to={`/product_detail/${product.id}`}>
+            <Card>
+              <img alt="Sample" src="https://picsum.photos/300/200" />
+              <CardBody>
+                <CardTitle tag="h5">Card title</CardTitle>
+                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                  {product.name}
+                </CardSubtitle>
+                <CardText>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card‘s content.
+                </CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+          </Link>
+        </div>
+      </Col>
+    
+  );
 }
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Product_detail from "../product_detail/Product_detail";
+import { Container, Row } from "reactstrap";
+
 
 export default function Product() {
-    return <div>Products</div>;
+    const [data, setData] = useState([]);
+    const url = "https://65980bfb668d248edf23ffb0.mockapi.io/hocsinh";
+    useEffect(()=>{
+        axios.get(url).then(function (res){
+            setData(res.data);
+        });
+    }, []);
+    return <div>
+        <Container>
+            <Row>
+                {data.map((item, index) => (
+                    <Product_detail key={index} product={item} />
+                ))}
+            </Row>
+        </Container>
+    </div>;
 }
